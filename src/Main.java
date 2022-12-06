ef67a0719c3bd9c299f1b401fb9777aacddb8f13
Tokimitsu <civicq@gmail.com>
2022-12-06 11:52:00 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -19,75 +19,75 @@ public class Main {
 
 
         // 1) Работа со String
-        String str1 = "Hello";
-        String str2 = "Mr. Vlad";
-        System.out.println(str1 + " " + str2);
-
-        String str3 = "Year of issue " + 2015;
-        System.out.println(str3);
-
-
-        String str = "Super-mega Long sentence";
-        char c = str.charAt(2);
-        System.out.println(c);
-
-        System.out.println(str.charAt(0));
-        System.out.println(str.charAt(1));
-        System.out.println(str.charAt(2));
-
-
-        String str4 = "Uniqueness";
-        String str5 = "uniqueness";
-
-        System.out.println(str4.equals(str5));
-        System.out.println(str4.equalsIgnoreCase(str5));
-
-        String str6 = "Hello world";
-        String replace = str6.replace('l', 'd');
-        System.out.println(replace);
-        String replace1 = str6.replace("Hello", "Bye");
-        System.out.println(replace1);
-
-        String str7 = "  hello Vlad  ";
-        str7 = str7.trim();
-        System.out.println(str7);
-
-
-        String str8 = "Java is the most interesting thing I've ever encountered";
-        System.out.println(str.toLowerCase());
-        System.out.println(str.toUpperCase());
-
-
-        // 2) Продемонстировать явное и неявное приведение типов
-                                                                                            // Явное
-        int a = 4;
-        byte b = (byte)a;
-        System.out.println(b);
-
-        double a1 = 56.9898;
-        int b1 = (int)a1;
-        double a2 = 56.9898;
-        int b2 = (int)Math.round(a2);
-
-                                                                                             // Неявное
-
-        byte b3 = 7;
-        int d1 = b3;
-
-        int a3 = 3;
-        double b4 = 4.6;
-        double c1 = a3+b4;
-
-// 3) Показать переполнение
-
-
-        int a4 = Integer.MAX_VALUE;
-        int a5 = a4 + 1000;
-        System.out.println(a5);
-
-        long l = (long)a4 + 11;
-        System.out.println(l);
-
+//        String str1 = "Hello";
+//        String str2 = "Mr. Vlad";
+//        System.out.println(str1 + " " + str2);
+//
+//        String str3 = "Year of issue " + 2015;
+//        System.out.println(str3);
+//
+//
+//        String str = "Super-mega Long sentence";
+//        char c = str.charAt(2);
+//        System.out.println(c);
+//
+//        System.out.println(str.charAt(0));
+//        System.out.println(str.charAt(1));
+//        System.out.println(str.charAt(2));
+//
+//
+//        String str4 = "Uniqueness";
+//        String str5 = "uniqueness";
+//
+//        System.out.println(str4.equals(str5));
+//        System.out.println(str4.equalsIgnoreCase(str5));
+//
+//        String str6 = "Hello world";
+//        String replace = str6.replace('l', 'd');
+//        System.out.println(replace);
+//        String replace1 = str6.replace("Hello", "Bye");
+//        System.out.println(replace1);
+//
+//        String str7 = "  hello Vlad  ";
+//        str7 = str7.trim();
+//        System.out.println(str7);
+//
+//
+//        String str8 = "Java is the most interesting thing I've ever encountered";
+//        System.out.println(str.toLowerCase());
+//        System.out.println(str.toUpperCase());
+//
+//
+//        // 2) Продемонстировать явное и неявное приведение типов
+//                                                                                            // Явное
+//        int a = 4;
+//        byte b = (byte)a;
+//        System.out.println(b);
+//
+//        double a1 = 56.9898;
+//        int b1 = (int)a1;
+//        double a2 = 56.9898;
+//        int b2 = (int)Math.round(a2);
+//
+//                                                                                             // Неявное
+//
+//        byte b3 = 7;
+//        int d1 = b3;
+//
+//        int a3 = 3;
+//        double b4 = 4.6;
+//        double c1 = a3+b4;
+//
+//// 3) Показать переполнение
+//
+//
+//        int a4 = Integer.MAX_VALUE;
+//        int a5 = a4 + 1000;
+//        System.out.println(a5);
+//
+//        long l = (long)a4 + 11;
+//        System.out.println(l);
+//
 
 // 4) Показать работу с методами и классами
 
@@ -97,11 +97,11 @@ String_edit string_edit = new String_edit();
 
 
 // 5*) Сделать реверс строки
-
-String rev = "This is reversed phrase!";
-        for (int i = rev.length() - 1; i >= 0; i--) {
-            System.out.print(rev.charAt(i));
-        }
+//
+//String rev = "This is reversed phrase!";
+//        for (int i = rev.length() - 1; i >= 0; i--) {
+//            System.out.print(rev.charAt(i));
+//        }
 
 
 
--- src/String_edit.java
+++ src/String_edit.java
@@ -7,11 +7,7 @@ static Scanner scanner = new Scanner(System.in);
 
 
         System.out.println("Guess the animal that lives in a swamp and has green color");
-//        System.out.println("Press 1 for Lion");
-//        System.out.println("Press 2 for Frog");
-//        System.out.println("Press 3 for Horse");
 
-//        String a = "Choose the correct number";
         String b = "Congratulations, you have guessed the correct answer";
         String c = "Sadly, but you are wrong";
 
